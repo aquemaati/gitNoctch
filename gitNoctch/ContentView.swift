826896846b7788b640080213a05be4abfc3085cd
aquemaati <aquemaati@gmail.com>
2026-05-28 14:13:23 +0200

    Initial Commit

---
+++ gitNoctch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitNoctch
+//
+//  Created by Mathias Marchetti on 28/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitNoctch/gitNoctchApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitNoctchApp.swift
+//  gitNoctch
+//
+//  Created by Mathias Marchetti on 28/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct gitNoctchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
